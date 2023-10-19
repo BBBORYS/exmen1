3bda94a3bc3323908af9945b3eab3873ed61dc6c
BORYS <94471361+BBBORYS@users.noreply.github.com>
2023-10-19 16:09:17 -0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,63 @@
+export interface IFuction {
+    id: number;
+    name: string;
+    returnType: string;
+    status: string;
+    parameters: IParameter[];
+  }
+  
+  export interface IParameter {
+    id: number;
+    name: string;
+    returnType: string; 
+    status: string;
+    parameters: IParameter[];
+  }
+  
+
+  const funciones: IFuction[] = [
+    {
+      id: 1,
+      name: 'Funcion1',
+      returnType: 'void',
+      status: 'activa',
+      parameters: []
+    },
+    {
+      id: 2, 
+      name: 'Funcion2',
+      returnType: 'string',
+      status: 'inactiva',
+      parameters: [
+        { 
+          id: 1,
+          name: 'parametro1',
+          returnType: 'number',
+          status: 'requerido',
+          parameters: []  
+        }
+      ]
+    },
+    {
+      id: 3,
+      name: 'Funcion3',
+      returnType: 'boolean',
+      status: 'activa',
+      parameters: [
+        {
+          id: 1,
+          name: 'parametro1',
+          returnType: 'string',
+          status: 'opcional',
+          parameters: []
+        },
+        {
+          id: 2,
+          name: 'parametro2',
+          returnType: 'number',
+          status: 'requerido',
+          parameters: []
+        } 
+      ]
+    }
+  ];
